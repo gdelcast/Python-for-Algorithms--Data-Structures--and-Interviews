8d5c64ff905bf14975a0773f3321324213e2977e
Guillermo Del Castillo <38362781+gdelcast@users.noreply.github.com>
2021-09-26 17:45:01 -0700

    .

--- Sorting and Searching/1. bubblesort.js
+++ Sorting and Searching/1. bubblesort.js
@@ -1,8 +1,8 @@
 function bubble_sort (arr){
     
-    for (var i=0 ; i< arr.length; i++){
-        for (var j=0 ; j<arr.length-1; j++){
-            if(arr[j]>arr[j+1]){
+    for (var i=0 ; i< arr.length; i++){                 // loop parse all elements
+        for (var j=0 ; j<arr.length-1; j++){            // nested loop all elements
+            if(arr[j]>arr[j+1]){                        // compare j and j+1 and swaps
                 const temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
--- Sorting and Searching/2. selectionsort.js
+++ Sorting and Searching/2. selectionsort.js
@@ -1,14 +1,14 @@
 function selection_sort (arr){
     
-    for (var i=0 ; i< arr.length; i++){
+    for (var i=0 ; i< arr.length; i++){         // loop all elements in array
         let min = i;
 
-        for (var j=i ; j<arr.length; j++){
+        for (var j=i ; j<arr.length; j++){      // nested loop searches the min value on the remaining portion of arr
             if(arr[j]<arr[min]){
                 min = j;
             }
         }
-        const temp = arr[i];
+        const temp = arr[i];                    // swaps current with min value
         arr[i] = arr[min];
         arr[min] = temp;
     }
