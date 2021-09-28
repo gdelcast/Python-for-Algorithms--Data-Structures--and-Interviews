a30f88515afe5398e90080418edab4b966b9f72a
Guillermo Del Castillo <38362781+gdelcast@users.noreply.github.com>
2021-09-28 10:03:17 -0700

    .

--- Sorting and Searching/0.binarysearch.py
+++ Sorting and Searching/0.binarysearch.py
@@ -4,13 +4,13 @@ def binary_search(arr,ele):
     if len(arr)<1:
         return None
     
-    if len(arr)==1 and arr[0]==ele:
-        return True
-    elif len(arr)==1 and arr[0]!=ele:
+    if len(arr)==1 and arr[0]!=ele:
         return False
     else:
         middle = (len(arr)-1)//2
-        if ele<arr[middle]:
+        if ele == arr[middle]:
+            return True
+        elif ele<arr[middle]:
             return binary_search(arr[:middle],ele)
         else:
             return binary_search(arr[middle:],ele)
@@ -18,4 +18,4 @@ def binary_search(arr,ele):
 
 
 arr = [1, 2, 5, 3, 4, 6, 8, 12, 25, 41]
-print(binary_search(arr,12))
+print(binary_search(arr,9))
