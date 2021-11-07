103c7d6f64ad5cecd3ffd8114821db02e2c83fea
Guillermo Del Castillo <38362781+gdelcast@users.noreply.github.com>
2021-11-07 15:27:11 -0500

    .

--- GuillermoS/num_islands_w_Areasize.py
+++ GuillermoS/num_islands_w_Areasize.py
@@ -1,3 +1,4 @@
+from collections import deque
 def  num_islands(grid):
     if not grid: return
     count = 0
@@ -11,7 +12,8 @@ def  num_islands(grid):
             if grid[i][j] == '1':
                 count+=1
                 area[count] = 0
-                __visitIsland(grid, i, j, area, count)
+                #__visitIsland(grid, i, j, area, count)
+                __visitBFS(grid, i, j)
 
     return [count, max(area.values())]  #return # of islands and area of largest island
 
@@ -27,6 +29,37 @@ def __visitIsland(grid, i, j, area, count):
             __visitIsland(grid, i+1, j, area, count)
         if j+1<len(grid[0]):
             __visitIsland(grid, i, j+1, area, count)
+
+def __visitBFS(grid, i, j):        
+        nr = len(grid)
+        nc = len(grid[0])
+
+        if grid[i][j]=='1':
+
+            grid[i][j]=='0'
+            neighbors = deque([])
+            neighbors.append(i * nc + j)
+            
+            while neighbors:
+                n = neighbors.popleft()
+                ni = n//nc
+                nj = n%nc
+                
+                if ni-1>=0 and grid[ni-1][nj] =='1':
+                    grid[ni-1][nj] ='0'
+                    neighbors.append((ni-1)*nc+nj)
+                    
+                if ni+1<nr and grid[ni+1][nj] =='1':
+                    grid[ni+1][nj] ='0'
+                    neighbors.append((ni+1)*nc+nj)
+                    
+                if nj-1>=0 and grid[ni][nj-1] =='1':
+                    grid[ni][nj-1] ='0'
+                    neighbors.append(ni*nc+(nj-1))
+                    
+                if nj+1<nc and grid[ni][nj+1] =='1':
+                    grid[ni][nj+1] ='0'
+                    neighbors.append(ni*nc+(nj+1))
 
 
 grid = []
